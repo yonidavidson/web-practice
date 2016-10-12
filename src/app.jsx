8447aa573773c2e1390be692fb1af91bcac56d98
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-12 21:35:59 +0300

    added Add capability

--- src/app.jsx
+++ src/app.jsx
@@ -11,15 +11,33 @@ const Recipes = ({ recipes }) => (
   </ul>
 );
 
-const recipes = ['Andrew G', 'Eyal K'];
+const recipes = ['Andrew G', 'Eyal K', 'Yoni D'];
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    
+    this.state = {
+      recipes: recipes
+    }  
+  }
+  
   render() {
     return (
       <div>
         <h1> list of names ! </h1>
-        <Recipes recipes={ recipes } />
+        <form onSubmit={ this.onSubmit.bind(this)}>
+          <input ref="title" type="text"/>
+          <button>Add</button>
+        </form>
+        <Recipes recipes={ this.state.recipes } />
       </div>
     )
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+    this.setState({recipes: this.state.recipes.concat(this.refs.title.value)})
+    this.refs.title.value = ''
   }
 }
