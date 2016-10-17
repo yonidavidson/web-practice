38d399aa131376139a9a9ead56def606aa0f6086
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-17 23:50:13 +0300

    add edit state to edit button

--- src/components/editName.jsx
+++ src/components/editName.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
-const EditName = ({name}) => (
-	<button style={ {margin:'10px'} }> edit </button>
-)
+class EditName extends React.Component{
+  constructor(){
+    super()
+    this.state = {edited : false}
+  }
+  render(){
+    return(
+        <button style={ {margin:'10px'} }  onClick={this.onClick.bind(this)}> edit </button>      
+    )
+  }
+
+  onClick(e){
+  	console.log(this.state.edited)
+    this.setState({edited: !this.state.edited})
+  }
+}
+
 
 export default EditName
