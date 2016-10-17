3a371be69ab33db63f36b2eea11f2166a27bc54e
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-17 21:52:08 +0300

    allow adding same name more then once

--- src/reducers/root.jsx
+++ src/reducers/root.jsx
@@ -3,7 +3,9 @@ import {ADD_NAME} from '../consts/action-types.jsx'
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_NAME:
-    return Object.assign({}, state, { names: state.names.concat(action.title) } )
+    return state.names.includes(action.title)?
+    		Object.assign({}, state):
+    		Object.assign({}, state, { names: state.names.concat(action.title) } )
   }
   return state
 }
