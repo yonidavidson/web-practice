03aa2538ab698350f65dd9230a3800a56af75bda
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-22 14:53:24 +0300

    fetch names via async function

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,10 +1,10 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import reducer from '../reducers/root.jsx'
-
+import thunk from 'redux-thunk'
 
 const initialState = {
   names: []
 }
 
 
-export default createStore(reducer, initialState)
+export default createStore(reducer, initialState,applyMiddleware(thunk))
