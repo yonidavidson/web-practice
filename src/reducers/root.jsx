291e7c1cbcc390f4744907355cd70d51cc260272
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-23 01:42:18 +0300

    clean add name

--- src/reducers/root.jsx
+++ src/reducers/root.jsx
@@ -3,9 +3,7 @@ import {ADD_NAME, REMOVE_NAME, EDIT_NAME, GET_NAMES} from '../consts/action-type
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_NAME:
-      return state.names.includes(action.title)?
-  		Object.assign({}, state):
-  		Object.assign({}, state, { names: state.names.concat(action.title) } )
+      return Object.assign({}, state, { names: state.names.concat(action.title) } )
     case REMOVE_NAME:
     	return Object.assign({}, {names: state.names.filter(v => action.title !== v)})
     case EDIT_NAME:
