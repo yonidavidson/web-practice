2811eaed9f2f2232da8f97bd49ef1d764aed1df9
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-22 21:56:34 +0300

    post can add only once

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 const VERSION = 1;
 
-const db = ["Fortuna", "Moka", "Karashindo" , "Tyron"];
+let db = ["Fortuna", "Moka", "Karashindo" , "Tyron"];
 
 app.use(bodyParser.json())
 
@@ -14,10 +14,14 @@ app.get('/', (req, res) => res.send('Version:' + VERSION))
 app.get('/names', (req, res) => res.send(db))
 
 app.post('/names', (req, res) =>{
-	const name = req.body
-	db.concat(name)
-	const location = '/names/' + name
-	res.set('location','/names/testName').status(201).send()
+	const name = req.body.name
+	if (db.includes(name)){
+		res.status(409).send()	
+	}else{
+		db = db.concat(name)
+		const location = '/names/' + name
+		res.set('location',location).status(201).send()	
+	}
 })
 
 app.listen(PORT, () => console.log('server started on PORT:', PORT));
--- test/server.js
+++ test/server.js
@@ -4,6 +4,7 @@ const request = require('supertest')('http://localhost:3000');
 
 describe('Server tests', () => {
 	const VERSION_RESPONSE = "Version:1"
+	const NAME_1 = "lola"
 	it('should get'+ VERSION_RESPONSE, (done) => {
 		request.
 		get('/').
@@ -28,15 +29,23 @@ describe('Server tests', () => {
 	it('should add a name', (done) => {
 		request.
 		post('/names').
+		send({name: NAME_1}).
 		expect(201).
 		end((err, response) => {
 			if (err) {
 				throw err
 			}
-			expect(response.header).to.have.property('location','/names/testName')
+			expect(response.header).to.have.property('location','/names/'+ NAME_1)
 			done()
 		})
 		
+	})
+
+	it('should give error for trying to add same resource more then once', (done) => {
+		request.
+		post('/names').
+		send({name: NAME_1}).
+		expect(409,'',done)
 	})
 
 //end of tests
