d226204af46cf0e51372e34ab60caee7ce475bba
Yoni Davidson <yoni.davidson@eyesight-tech.com>
2016-10-22 20:27:51 +0300

    add get names

--- src/server/index.js
+++ src/server/index.js
@@ -5,10 +5,12 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 const VERSION = 1;
 
-const db = [];
+const db = ["Fortuna", "Moka", "Karashindo" , "Tyron"];
 
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Version:' + VERSION))
+
+app.get('/names', (req, res) => res.send(db))
 
 app.listen(PORT, () => console.log('server started on PORT:', PORT));
--- test/server.js
+++ test/server.js
@@ -3,12 +3,27 @@ const expect = require('chai').expect;
 const request = require('supertest')('http://localhost:3000');
 
 describe('Server tests', () => {
-const VERSION_RESPONSE = "Version:1"
-  it('should get'+ VERSION_RESPONSE, (done) => {
-    request.
-    get('/').
-    expect(200, VERSION_RESPONSE, done);
-  })
+	const VERSION_RESPONSE = "Version:1"
+	it('should get'+ VERSION_RESPONSE, (done) => {
+		request.
+		get('/').
+		expect(200, VERSION_RESPONSE, done);
+	})
+
+	it ('should get list of names', (done) =>{
+		request.
+		get('/names').
+		expect(200).
+		end((err, response) =>{
+			if (err) {
+				throw err
+			}
+			expect(response.body).to.be.instanceof(Array)
+			expect(response.body[0]).to.be.an('string')
+			done()	
+		})
+
+	})
 
 //end of tests
 })
